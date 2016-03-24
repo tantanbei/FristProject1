0478a6a128746e398db14c4f5adf769c1dd4c64d
tankaide <tkd@lbxtech.com>
2016-03-24 22:07:32 +0800

    ADD MPAndroidChart packet

--- app/src/main/java/fristproject1/sample/com/fristproject1/MainActivity.java
+++ app/src/main/java/fristproject1/sample/com/fristproject1/MainActivity.java
@@ -1,5 +1,6 @@
 package fristproject1.sample.com.fristproject1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.iflytek.cloud.Setting;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechUtility;
@@ -23,132 +27,16 @@ import com.iflytek.cloud.RecognizerListener;
 import com.iflytek.cloud.RecognizerResult;
 import com.iflytek.cloud.SpeechError;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=562f728d");
-
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // in this example, a LineChart is initialized from xml
+        LineChart chart = (LineChart) findViewById(R.id.chart);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-//        SpeechRecognizer mIat= SpeechRecognizer.createRecognizer(MainActivity.this, null);
-//        mIat.setParameter(SpeechConstant.DOMAIN, "iat");
-//        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
-//        mIat.setParameter(SpeechConstant.ACCENT, "mandarin ");
-//        mIat.startListening(mRecoListener);
-
-        Button bt;
-        bt=(Button)findViewById(R.id.RecognizeViewBtn);
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("Result:","on click!");
-                SpeechRecognizer mIat= SpeechRecognizer.createRecognizer(MainActivity.this, null);
-                mIat.setParameter(SpeechConstant.DOMAIN, "iat");
-                mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
-                mIat.setParameter(SpeechConstant.ACCENT, "mandarin ");
-                mIat.startListening(mRecoListener);
-            }
-        });
-    }
-
-    private RecognizerListener mRecoListener = new RecognizerListener(){
-        public void onBeginOfSpeech() {
-            Log.d("Result:","start speech!");
-        }
-        public void onResult(RecognizerResult results, boolean isLast) {
-            Log.d("Result:", results.getResultString());
-        }
-        public void onError(SpeechError error) {
-            Log.d("Error:",error.getPlainDescription(true));
-        }
-
-        public void onVolumeChanged(int volume, byte[] data){}
-
-        public void onEndOfSpeech() {
-            Log.d("Result:","end speech!");
-        }
-
-        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {}
-    };
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
