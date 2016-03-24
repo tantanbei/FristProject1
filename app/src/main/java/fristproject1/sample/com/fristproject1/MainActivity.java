f6328e7f641f1fed9b70b2408b42a49e68add325
tankaide <tkd@lbxtech.com>
2016-03-24 22:28:14 +0800

    MOD some example

--- app/src/main/java/fristproject1/sample/com/fristproject1/MainActivity.java
+++ app/src/main/java/fristproject1/sample/com/fristproject1/MainActivity.java
@@ -18,6 +18,11 @@ import android.widget.Button;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.iflytek.cloud.Setting;
 import com.iflytek.cloud.SpeechConstant;
@@ -26,6 +31,8 @@ import com.iflytek.cloud.SpeechRecognizer;
 import com.iflytek.cloud.RecognizerListener;
 import com.iflytek.cloud.RecognizerResult;
 import com.iflytek.cloud.SpeechError;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -37,6 +44,28 @@ public class MainActivity extends AppCompatActivity{
 
         // in this example, a LineChart is initialized from xml
         LineChart chart = (LineChart) findViewById(R.id.chart);
+
+
+        ArrayList<Entry> valsComp1 = new ArrayList<Entry>();
+        Entry c1e1 = new Entry(100.000f, 0); // 0 == quarter 1
+        valsComp1.add(c1e1);
+        Entry c1e2 = new Entry(50.000f, 1); // 1 == quarter 2 ...
+        valsComp1.add(c1e2);
+
+        LineDataSet setComp1 = new LineDataSet(valsComp1, "Company 1");
+        setComp1.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(setComp1);
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("1.Q"); xVals.add("2.Q"); xVals.add("3.Q"); xVals.add("4.Q");
+
+        LineData data = new LineData(xVals, dataSets);
+
+        chart.setData(data);
+
+        chart.invalidate();
 
     }
 }
