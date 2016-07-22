e41ba60af20a1ff7d28eaff0ada39dd843ecb292
tantanbei <tan1115@outlook.com>
2016-07-22 22:13:38 +0800

    MDO cannnot trun screen orientation

--- app/src/main/java/fristproject1/sample/com/fristproject1/fragment/HomeTabFragment.java
+++ app/src/main/java/fristproject1/sample/com/fristproject1/fragment/HomeTabFragment.java
@@ -31,6 +31,7 @@ import fristproject1.sample.com.fristproject1.activity.ActivityAuctionIdle;
 import fristproject1.sample.com.fristproject1.activity.ActivityHistoryData;
 import fristproject1.sample.com.fristproject1.activity.ActivityHome;
 import fristproject1.sample.com.fristproject1.networkpacket.AuctionStatus;
+import fristproject1.sample.com.fristproject1.thread.XThread;
 import fristproject1.sample.com.fristproject1.time.XTime;
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
@@ -40,6 +41,8 @@ import okhttp3.Response;
 public class HomeTabFragment extends Fragment {
 
     Activity Parent;
+
+    Runnable refreshRunnable;
 
     private IconTextView homeMenu;
     private IconTextView homeCustomerService;
@@ -77,8 +80,6 @@ public class HomeTabFragment extends Fragment {
         readyForecast = (TextView) view.findViewById(R.id.ready_forecast);
         runningForecast = (TextView) view.findViewById(R.id.running_forecast);
         overPrice = (TextView) view.findViewById(R.id.over_price);
-
-        refreshData();
 
         for (int i = 0; i < srcIds.length; i++) {
             ImageView imageView = new ImageView(getContext());
@@ -131,99 +132,109 @@ public class HomeTabFragment extends Fragment {
         return view;
     }
 
-    private void refreshData() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Request request = new Request.Builder()
-                        .url(Const.SERVER_IP + "/auction/status")
-                        .method("GET", null)
-                        .build();
+    @Override
+    public void onStart() {
+        super.onStart();
+        refreshData(0);
+    }
 
-                try {
-                    final Response response = client.newCall(request).execute();
-                    final String str = response.body().string();
-                    Log.d("tan", "refreshData: " + str);
+    private void refreshData(long delay) {
 
-                    final AuctionStatus status = LoganSquare.parse(str, AuctionStatus.class);
+        if (refreshRunnable == null) {
+            refreshRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    Request request = new Request.Builder()
+                            .url(Const.SERVER_IP + "/auction/status")
+                            .method("GET", null)
+                            .build();
 
-                    App.Uihandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            switch (status.Id) {
-                                case 0:
-                                    auctionIdle.setVisibility(View.VISIBLE);
-                                    auctionReady.setVisibility(View.GONE);
-                                    auctionRunning.setVisibility(View.GONE);
-                                    auctionOver.setVisibility(View.GONE);
-                                    if (status.Data.length == 1) {
-                                        long time = status.Data[0];
-                                        int day = XTime.GetDayByTimeStamp(time);
-                                        Log.d("tan", "day: " + day);
-                                        idleDate.setText(day + "号");
-                                    }
-                                    break;
-                                case 1:
-                                    auctionIdle.setVisibility(View.GONE);
-                                    auctionReady.setVisibility(View.VISIBLE);
-                                    auctionRunning.setVisibility(View.GONE);
-                                    auctionOver.setVisibility(View.GONE);
-                                    if (status.Data.length == 2) {
-                                        long time = status.Data[0];
-                                        int timeGap = (int) ((time - System.currentTimeMillis()));
-                                        int day = timeGap / (1000 * 60 * 60 * 24);
+                    try {
+                        final Response response = client.newCall(request).execute();
+                        final String str = response.body().string();
+                        Log.d("tan", "refreshData: " + str);
 
-                                        if (day != 0) {
-                                            readyDate.setText(day + "天");
-                                        } else {
-                                            int hour = timeGap / (1000 * 60 * 60);
+                        final AuctionStatus status = LoganSquare.parse(str, AuctionStatus.class);
 
-                                            if (hour != 0) {
-                                                readyDate.setText(hour + "小时");
+                        App.Uihandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                switch (status.Id) {
+                                    case 0:
+                                        auctionIdle.setVisibility(View.VISIBLE);
+                                        auctionReady.setVisibility(View.GONE);
+                                        auctionRunning.setVisibility(View.GONE);
+                                        auctionOver.setVisibility(View.GONE);
+                                        if (status.Data.length == 1) {
+                                            long time = status.Data[0];
+                                            int day = XTime.GetDayByTimeStamp(time);
+                                            Log.d("tan", "day: " + day);
+                                            idleDate.setText(day + "号");
+                                        }
+                                        break;
+                                    case 1:
+                                        auctionIdle.setVisibility(View.GONE);
+                                        auctionReady.setVisibility(View.VISIBLE);
+                                        auctionRunning.setVisibility(View.GONE);
+                                        auctionOver.setVisibility(View.GONE);
+                                        if (status.Data.length == 2) {
+                                            long time = status.Data[0];
+                                            int timeGap = (int) ((time - System.currentTimeMillis()));
+                                            int day = timeGap / (1000 * 60 * 60 * 24);
+
+                                            if (day != 0) {
+                                                readyDate.setText(day + "天");
                                             } else {
-                                                int min = timeGap / (1000 * 60);
-                                                if (min != 0) {
-                                                    readyDate.setText(min + "分钟");
+                                                int hour = timeGap / (1000 * 60 * 60);
+
+                                                if (hour != 0) {
+                                                    readyDate.setText(hour + "小时");
                                                 } else {
-                                                    int sec = timeGap / (1000);
-                                                    if (sec != 0) {
-                                                        readyDate.setText(sec + "秒");
-                                                        refreshData();
+                                                    int min = timeGap / (1000 * 60);
+                                                    if (min != 0) {
+                                                        readyDate.setText(min + "分钟");
+                                                    } else {
+                                                        int sec = timeGap / (1000);
+                                                        if (sec != 0) {
+                                                            readyDate.setText(sec + "秒");
+                                                            refreshData(600);
+                                                        }
                                                     }
                                                 }
                                             }
+
+                                            readyForecast.setText(status.Data[1] + "-" + (status.Data[1] + 300));
                                         }
-
-
-                                        readyForecast.setText(status.Data[1] + "-" + (status.Data[1] + 300));
-                                    }
-                                    break;
-                                case 2:
-                                    auctionIdle.setVisibility(View.GONE);
-                                    auctionReady.setVisibility(View.GONE);
-                                    auctionRunning.setVisibility(View.VISIBLE);
-                                    auctionOver.setVisibility(View.GONE);
-                                    if (status.Data.length == 1) {
-                                        runningForecast.setText(String.valueOf(status.Data[0]));
-                                    }
-                                    break;
-                                case 3:
-                                    auctionIdle.setVisibility(View.GONE);
-                                    auctionReady.setVisibility(View.GONE);
-                                    auctionRunning.setVisibility(View.GONE);
-                                    auctionOver.setVisibility(View.VISIBLE);
-                                    if (status.Data.length == 1) {
-                                        overPrice.setText(String.valueOf(status.Data[0]));
-                                    }
-                                    break;
+                                        break;
+                                    case 2:
+                                        auctionIdle.setVisibility(View.GONE);
+                                        auctionReady.setVisibility(View.GONE);
+                                        auctionRunning.setVisibility(View.VISIBLE);
+                                        auctionOver.setVisibility(View.GONE);
+                                        if (status.Data.length == 1) {
+                                            runningForecast.setText(String.valueOf(status.Data[0]));
+                                        }
+                                        break;
+                                    case 3:
+                                        auctionIdle.setVisibility(View.GONE);
+                                        auctionReady.setVisibility(View.GONE);
+                                        auctionRunning.setVisibility(View.GONE);
+                                        auctionOver.setVisibility(View.VISIBLE);
+                                        if (status.Data.length == 1) {
+                                            overPrice.setText(String.valueOf(status.Data[0]));
+                                        }
+                                        break;
+                                }
                             }
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        }).start();
+            };
+        }
+
+        XThread.RunBackground(refreshRunnable, delay);
     }
 
     @Override
@@ -260,6 +271,15 @@ public class HomeTabFragment extends Fragment {
         public Object instantiateItem(ViewGroup container, int position) {
             container.addView(ImageArrayList.get(position % ImageArrayList.size()), 0);
             return ImageArrayList.get(position % ImageArrayList.size());
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if (refreshRunnable != null) {
+            XThread.Cancel(refreshRunnable);
         }
     }
 }
