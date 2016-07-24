9b62ec187e4820ce3e8f5c8a5b337b9bf37e5a20
tantanbei <tan1115@outlook.com>
2016-07-24 23:45:28 +0800

    ADD the sign up layout

--- app/src/main/java/fristproject1/sample/com/fristproject1/String/XString.java
+++ app/src/main/java/fristproject1/sample/com/fristproject1/String/XString.java
@@ -11,4 +11,12 @@ public class XString {
     static public String GetString(Context context, @StringRes int id){
         return context.getString(id);
     }
+
+    static public boolean IsEmpty(String str){
+        if (str == null || str.length() == 0 || str.equals("")){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
---
+++ app/src/main/java/fristproject1/sample/com/fristproject1/activity/ActivitySignUp.java
@@ -0,0 +1,57 @@
+package fristproject1.sample.com.fristproject1.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import fristproject1.sample.com.fristproject1.R;
+import fristproject1.sample.com.fristproject1.String.XString;
+
+public class ActivitySignUp extends Activity{
+
+    EditText phone;
+    EditText code;
+    EditText password;
+    EditText confirmPassword;
+    Button getCode;
+    Button signUp;
+
+    String verificationCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup);
+
+        phone = (EditText) findViewById(R.id.phone);
+        code = (EditText) findViewById(R.id.code);
+        password = (EditText) findViewById(R.id.password);
+        confirmPassword = (EditText) findViewById(R.id.confirm_password);
+        getCode = (Button) findViewById(R.id.get_verification_code);
+        signUp = (Button) findViewById(R.id.sign_up);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (XString.IsEmpty(code.getText().toString())){
+                    Toast.makeText(ActivitySignUp.this,XString.GetString(ActivitySignUp.this,R.string.input_verification_code),Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!code.getText().toString().equals(verificationCode)){
+                    Toast.makeText(ActivitySignUp.this, R.string.verification_error,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!password.getText().toString().equals(confirmPassword.getText().toString())){
+                    Toast.makeText(ActivitySignUp.this, R.string.confirm_password_error,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+    }
+}
