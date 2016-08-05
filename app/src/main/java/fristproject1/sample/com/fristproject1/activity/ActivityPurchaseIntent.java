7f1552adf7320be995c3b2e799a6f57d68d4f59f
tantanbei <tan1115@outlook.com>
2016-08-05 21:03:10 +0800

    MOD separate the addCarLayout from orginal layout

--- app/src/main/java/fristproject1/sample/com/fristproject1/activity/ActivityPurchaseIntent.java
+++ app/src/main/java/fristproject1/sample/com/fristproject1/activity/ActivityPurchaseIntent.java
@@ -2,11 +2,12 @@ package fristproject1.sample.com.fristproject1.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import fristproject1.sample.com.fristproject1.R;
 import fristproject1.sample.com.fristproject1.activity.base.XActivity;
 
-public class ActivityPurchaseIntent extends XActivity{
+public class ActivityPurchaseIntent extends XActivity {
     @Override
     public int GetContentView() {
         return R.layout.activity_purchase_intent;
@@ -20,5 +21,11 @@ public class ActivityPurchaseIntent extends XActivity{
 
         rigthBtn.setVisibility(View.VISIBLE);
         rigthBtn.setText(R.string.skip);
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.add_car_layout);
+
+        //add the first addCarView
+        LinearLayout addView = (LinearLayout) getLayoutInflater().inflate(R.layout.add_car, null);
+        ll.addView(addView);
     }
 }
