fe96b499b806ce744bfca25ff3d4fa1feacdf228
tantanbei <tan1115@outlook.com>
2016-08-22 20:51:23 +0800

    ADD the new forecast auction price

---
+++ app/src/main/java/fristproject1/sample/com/fristproject1/activity/ActivityAuctionStrategy.java
@@ -0,0 +1,22 @@
+package fristproject1.sample.com.fristproject1.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.jmedeisis.draglinearlayout.DragLinearLayout;
+
+import fristproject1.sample.com.fristproject1.R;
+import fristproject1.sample.com.fristproject1.activity.base.XActivity;
+
+public class ActivityAuctionStrategy extends XActivity {
+    @Override
+    public int GetContentView() {
+        return R.layout.activity_auction_strategy;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
