a968c445794960a356347661e8ae0cf9fc2102b0
tantanbei <tan1115@outlook.com>
2016-08-22 23:31:45 +0800

    MOD little change

--- app/src/main/java/fristproject1/sample/com/fristproject1/activity/ActivityAuctionStrategy.java
+++ app/src/main/java/fristproject1/sample/com/fristproject1/activity/ActivityAuctionStrategy.java
@@ -2,13 +2,18 @@ package fristproject1.sample.com.fristproject1.activity;
 
 import android.os.Bundle;
 import android.view.View;
-
-import com.jmedeisis.draglinearlayout.DragLinearLayout;
+import android.widget.TextView;
 
 import fristproject1.sample.com.fristproject1.R;
 import fristproject1.sample.com.fristproject1.activity.base.XActivity;
 
 public class ActivityAuctionStrategy extends XActivity {
+
+    TextView second30;
+    TextView second40;
+    TextView second45;
+    TextView second50;
+
     @Override
     public int GetContentView() {
         return R.layout.activity_auction_strategy;
@@ -17,6 +22,11 @@ public class ActivityAuctionStrategy extends XActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        second30 = (TextView) findViewById(R.id.second_30);
+        second40 = (TextView) findViewById(R.id.second_40);
+        second45 = (TextView) findViewById(R.id.second_45);
+        second50 = (TextView) findViewById(R.id.second_50);
 
     }
 }
