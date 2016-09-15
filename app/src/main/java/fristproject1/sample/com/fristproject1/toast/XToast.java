f95068cf16279f5ba50a746bdac093a32e61cf8b
tantanbei <tan1115@outlook.com>
2016-09-16 00:52:36 +0800

    MOD xtoast use the xstack to get the context

--- app/src/main/java/fristproject1/sample/com/fristproject1/toast/XToast.java
+++ app/src/main/java/fristproject1/sample/com/fristproject1/toast/XToast.java
@@ -1,9 +1,28 @@
 package fristproject1.sample.com.fristproject1.toast;
 
 import android.content.Context;
+import android.widget.Toast;
+
+import fristproject1.sample.com.fristproject1.App;
+import fristproject1.sample.com.fristproject1.stack.XStack;
 
 public class XToast {
 
-    static public void Show(Context context, String str){
+    static public void Show(final String str) {
+        App.Uihandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(XStack.GetLastAliveActivity(), str, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    static public void ShowLong(final String str){
+        App.Uihandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(XStack.GetLastAliveActivity(), str, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
