9d3e128b1988bbcd4df8292ffff62d0e7d385191
tantanbei <tan1115@outlook.com>
2016-09-16 00:51:08 +0800

    MOD complete the xstack for cache the activity

--- app/src/main/java/fristproject1/sample/com/fristproject1/stack/XStack.java
+++ app/src/main/java/fristproject1/sample/com/fristproject1/stack/XStack.java
@@ -1,10 +1,13 @@
 package fristproject1.sample.com.fristproject1.stack;
 
+import android.app.Activity;
 import android.os.Build;
+import android.util.Log;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
+import fristproject1.sample.com.fristproject1.activity.ActivityAuction;
 import fristproject1.sample.com.fristproject1.activity.base.XActivity;
 
 public class XStack {
@@ -12,7 +15,7 @@ public class XStack {
     //max history size...
     public static final int MAX_HISTORY_SIZE;
 
-    private static ArrayList<WeakReference<XActivity>> history = new ArrayList<>();
+    private static ArrayList<WeakReference<Activity>> history = new ArrayList<>();
 
     static {
         //reduce stacksize for < 4.4 devices which are extremely buggy...
@@ -27,6 +30,47 @@ public class XStack {
             //android N
             MAX_HISTORY_SIZE = 12;
         }
+    }
+
+    private static void gc() {
+        if (history == null) {
+            history = new ArrayList<>();
+            return;
+        }
+
+        for (int i = 0; i < history.size(); i++) {
+            Activity a = history.get(i).get();
+            if (a == null || a.isDestroyed()) {
+                history.remove(0);
+                i--;
+            }
+        }
+    }
+
+    public static void Push(Activity a) {
+
+        gc();
+
+        Activity lastA = GetLastAliveActivity();
+
+        if (a != null && !a.isDestroyed()) {
+
+            //do not push duplicate item
+            if (lastA != a) {
+                history.add(new WeakReference<Activity>(a));
+            }
+        }
+    }
+
+    public static Activity GetLastAliveActivity() {
+        gc();
+
+        if (history == null || history.isEmpty()) {
+            Log.e("tan", "no alive avtivity on stack");
+            return null;
+        }
+
+        return history.get(history.size() - 1).get();
     }
 
 }
