6a60243bc1928436a3ee9dc67c77d21841f8d451
tantanbei <tan1115@outlook.com>
2016-12-02 23:01:50 +0800

    MOD update the layout on history and detail

--- app/src/main/java/com/whoplate/paipable/Const.java
+++ app/src/main/java/com/whoplate/paipable/Const.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Const {
 
-        static final public String SERVER_IP = "http://192.168.1.4";
-//    static final public String SERVER_IP = "http://114.55.174.165";
+//        static final public String SERVER_IP = "http://192.168.1.4";
+    static final public String SERVER_IP = "http://114.55.174.165";
 
     static final public String CUSTOMER_SERVICE_PHONE = "18117541072";
 
