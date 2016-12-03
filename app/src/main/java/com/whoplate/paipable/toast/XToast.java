a08ffbcfd598deffac2fe4da7d8927e398de4809
tantanbei <tan1115@outlook.com>
2016-12-04 00:20:42 +0800

    FIX make toast must be on ui thread

--- app/src/main/java/com/whoplate/paipable/toast/XToast.java
+++ app/src/main/java/com/whoplate/paipable/toast/XToast.java
@@ -12,78 +12,61 @@ public class XToast {
     static private Toast currToast;
 
     static public void Show(final String str) {
-        if (currToast != null) {
-            currToast.cancel();
-        }
-
-        currToast = Toast.makeText(XStack.GetLastAliveActivity(), str, Toast.LENGTH_SHORT);
-
-        if (XThread.IsUIThread()) {
-            currToast.show();
-        } else {
-            App.Uihandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    currToast.show();
+        App.Uihandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (currToast != null) {
+                    currToast.cancel();
                 }
-            });
-        }
+
+                currToast = Toast.makeText(XStack.GetLastAliveActivity(), str, Toast.LENGTH_SHORT);
+                currToast.show();
+            }
+        });
+
+
     }
 
     static public void Show(@StringRes final int resId) {
-        if (currToast != null) {
-            currToast.cancel();
-        }
-
-        currToast = Toast.makeText(XStack.GetLastAliveActivity(), resId, Toast.LENGTH_SHORT);
-
-        if (XThread.IsUIThread()) {
-            currToast.show();
-        } else {
-            App.Uihandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    currToast.show();
+        App.Uihandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (currToast != null) {
+                    currToast.cancel();
                 }
-            });
-        }
+
+                currToast = Toast.makeText(XStack.GetLastAliveActivity(), resId, Toast.LENGTH_SHORT);
+                currToast.show();
+            }
+        });
+
     }
 
     static public void ShowLong(final String str) {
-        if (currToast != null) {
-            currToast.cancel();
-        }
-
-        currToast = Toast.makeText(XStack.GetLastAliveActivity(), str, Toast.LENGTH_LONG);
-
-        if (XThread.IsUIThread()) {
-            currToast.show();
-        } else {
-            App.Uihandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    currToast.show();
+        App.Uihandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (currToast != null) {
+                    currToast.cancel();
                 }
-            });
-        }
+
+                currToast = Toast.makeText(XStack.GetLastAliveActivity(), str, Toast.LENGTH_LONG);
+                currToast.show();
+            }
+        });
     }
 
     static public void ShowLong(@StringRes final int resId) {
-        if (currToast != null) {
-            currToast.cancel();
-        }
-
-        currToast = Toast.makeText(XStack.GetLastAliveActivity(), resId, Toast.LENGTH_LONG);
-
-        if (XThread.IsUIThread()) {
-            currToast.show();
-        } else {
-            App.Uihandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    currToast.show();
+        App.Uihandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (currToast != null) {
+                    currToast.cancel();
                 }
-            });
-        }
+
+                currToast = Toast.makeText(XStack.GetLastAliveActivity(), resId, Toast.LENGTH_LONG);
+                currToast.show();
+            }
+        });
     }
 }
